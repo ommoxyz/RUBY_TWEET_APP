1dec8303972f6c0fcc3986f0b7600dba2888b9b0
ommoxyz <ommo.xyz@gmail.com>
2022-08-25 20:41:10 +0900

    ヘッダを共通化。投稿一覧画面を実装。

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  # ホーム
   get "/" => "home#top"
-  get "/about" => "home#about"
+  get "about" => "home#about"
+
+  # 投稿画面
+  get "posts/index" => "posts#index"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
