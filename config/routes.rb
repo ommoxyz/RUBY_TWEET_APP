11c77a4ffac3122303708bd1b4833bda943ca55e
ommoxyz <ommo.xyz@gmail.com>
2022-08-28 14:44:58 +0900

    編集機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
   get "posts/index" => "posts#index" # 投稿一覧
   get "posts/new" => "posts#new" # 新規投稿
   get "posts/:id" => "posts#show" # 投稿詳細
+  get "posts/:id/edit" => "posts#edit" # 投稿編集
   post "posts/create" => "posts#create" # 投稿登録
+  post "posts/:id/update" => "posts#update" # 投稿編集
 
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
