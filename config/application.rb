35442ae3c25920a013a718c5bcd610baee1b698e
ommoxyz <ommo.xyz@gmail.com>
2022-08-29 01:05:48 +0900

    コミットもれ

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,14 @@ module TweetApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # タイムゾーンの設定
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
+
+    # バリデーションエラーメッセージの日本語化
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
+
   end
 end
