0a0a7dd0afb4973c31d9297aabccba6fa2ac1c5e
ommoxyz <ommo.xyz@gmail.com>
2022-08-30 15:36:35 +0900

    ユーザアイコン登録・変更機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,12 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(name: params[:name], email: params[:email])
+    @user = User.new(
+      name: params[:name], 
+      email: params[:email],
+      image_name: "default_user.jpg"
+    )
+
     if @user.save
       redirect_to("/users/#{@user.id}")
       flash[:notice] = "ユーザー登録が完了しました"
@@ -29,6 +34,15 @@ class UsersController < ApplicationController
     @user = User.find_by(id: params[:id])
     @user.name = params[:name]
     @user.email = params[:email]
+
+    canSavedImage = false
+    if params[:image]
+      canSavedImage = File.binwrite("public/user_images/#{@user.id}.jpg", params[:image].read)
+    end
+
+    if canSavedImage
+      @user.image_name = "#{@user.id}.jpg"  
+    end
 
     if @user.save
       flash[:notice] = "ユーザー情報を編集しました"
